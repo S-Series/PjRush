db0f944c4cf1743c25bb66ac95062210c63c5e6e
SSeries <63185402+S-Series@users.noreply.github.com>
2021-10-18 11:28:24 +0900

    First Commit - 1

---
+++ Assets/0_Scenes/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem test;
+
+    private void Start()
+    {
+        test.Play();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            test.Play();
+        }
+    }
+}
