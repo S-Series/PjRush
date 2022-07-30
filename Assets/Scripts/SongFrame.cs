26a9968a1f8dd087d8cb3a8497e5566732440562
SSeries <63185402+S-Series@users.noreply.github.com>
2022-07-30 17:27:39 +0900

    2022.07.30 || 05:27PM

--- Assets/Scripts/SongFrame.cs
+++ Assets/Scripts/SongFrame.cs
@@ -5,10 +5,8 @@ using TMPro;
 
 public class SongFrame : MonoBehaviour
 {
-    [SerializeField]
-    static AudioSource Audio;
-    [SerializeField]
-    static AudioClip SecretAudio;
+    public AudioClip gameMusic;
+    public AudioClip gamePreMusic;
     // -----------------------------------------
     [SerializeField] private SpriteRenderer MusicJacket;
     [SerializeField] private SpriteRenderer[] MusicBlock;
@@ -19,13 +17,54 @@ public class SongFrame : MonoBehaviour
     public void SetFrame(Music music)
     {
         MusicJacket.sprite = music.sprJacket;
-        SongNameTmp.text = music.MusicName;
-        SongArtistTmp.text = music.MusicArtist;
-        for (int i = 0; i < 5; i++)
+        int index = SystemManager.difficultyIndex;
+        foreach (SpriteRenderer sprite in Difficulty) { sprite.gameObject.SetActive(false); }
+        foreach (SpriteRenderer sprite in MusicBlock) { sprite.enabled = false; }
+        if (music.isSecret)
         {
-            Difficulty[i].gameObject.SetActive(music.isAvailable[i]);
-            Difficulty[i].GetComponentInChildren<TextMeshPro>().text 
-                = string.Format("{d:02}", music.Difficulty[i]);
+            SongNameTmp.text = "????????";
+            SongArtistTmp.text = "????????";
+            for (int i = 0; i < 5; i++)
+            {
+                if (music.isAvailable[index])
+                {
+                    Difficulty[index].gameObject.SetActive(true);
+                    Difficulty[index].GetComponentInChildren<TextMeshPro>().text = "??";
+                }
+                index--;
+                if (index < 0) { index += 5; }
+            }
+            MusicBlock[1].enabled = true;
+            gameMusic = null;
+            gamePreMusic = null;
+        }
+        else
+        {
+            SongNameTmp.text = music.MusicName;
+            SongArtistTmp.text = music.MusicArtist;
+            for (int i = 0; i < 5; i++)
+            {
+                if (music.isAvailable[index])
+                {
+                    Difficulty[index].gameObject.SetActive(true);
+                    if (music.isOwned[4] && index == 4)
+                    {
+                        Difficulty[index].GetComponentInChildren<TextMeshPro>().text = "??";
+                        MusicBlock[0].enabled = true;
+                    }
+                    else
+                    {
+                        Difficulty[index].GetComponentInChildren<TextMeshPro>().text
+                            = string.Format("{0:D2}", music.Difficulty[index]);
+                        if (music.isOwned[index]) { MusicBlock[0].enabled = true; }
+                    }
+                    break;
+                }
+                index--;
+                if (index < 0) { index += 5; }
+            }
+            gameMusic = music.audMusicFile;
+            gamePreMusic = music.audPreMusicFile;
         }
         switch (music.status) //** Setting Dream Difficulty Sprite
         {
@@ -38,7 +77,5 @@ public class SongFrame : MonoBehaviour
                 Difficulty[4].sprite = SpriteManager.getDreamSprite(1);
                 break;
         }
-        
-
     }
 }
