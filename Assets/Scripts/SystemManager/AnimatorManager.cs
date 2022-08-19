dded2e36653d63d2ec86ec5099f3a2a7dc6d8e6f
SSeries <63185402+S-Series@users.noreply.github.com>
2022-08-19 17:47:43 +0900

    2022.08.19 || 05:47PM

--- Assets/Scripts/SystemManager/AnimatorManager.cs
+++ Assets/Scripts/SystemManager/AnimatorManager.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class AnimatorManager : MonoBehaviour
 {
-    private static List<Animator> animators = new List<Animator>();
     private static string[] trigger = {"Start", "End"};
-    [SerializeField] GameObject[] animatorObject;
+    [SerializeField] private GameObject[] animatorObject;
+    private static List<Animator> animators = new List<Animator>();
+    [SerializeField] private GameObject _spriteRenderer; 
+    private static SpriteRenderer JacketRenderer; 
     private void Awake()
     {
         for (int i = 0; i < animatorObject.Length; i++)
             { animators.Add(animatorObject[i].GetComponent<Animator>()); }
+        JacketRenderer = _spriteRenderer.GetComponent<SpriteRenderer>();
     }
     public static void PlayAnimation(int index, bool isStart)
     {
         if (isStart) animators[index].SetTrigger(trigger[0]);
         else animators[index].SetTrigger(trigger[1]);
+    }
+    public static void ChangeJacket(Sprite _sprite)
+    {
+        JacketRenderer.sprite = _sprite;
     }
 }
